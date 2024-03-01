7e56aeeb8e59c2741cbca575710f63cda45ad37b
Oláh Levente <levente@stud.uni-obuda.hu>
2024-03-01 15:21:33 +0100

    test setup round 2

--- CPP_Feladat_01/UnitTest1/UnitTest1.cpp
+++ CPP_Feladat_01/UnitTest1/UnitTest1.cpp
@@ -2,6 +2,8 @@
 #include "CppUnitTest.h"
 #include "feladat1.h"
 
+#define ARE_EQUAL(expVal, actVal) Assert::AreEqual(expVal, actVal, L"-", LINE_INFO())
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest1
@@ -12,6 +14,84 @@ namespace UnitTest1
 
 		TEST_METHOD(TestMethod1)
 		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
 		}
+		TEST_METHOD(TestMethod2)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod4)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod5)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod6)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod7)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod8)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod9)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod10)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod11)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod12)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod13)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod14)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod15)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod16)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod17)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod18)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod19)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+		TEST_METHOD(TestMethod20)
+		{
+			ARE_EQUAL(-1234.5678, feladat1("\n\t-1234.5678"));
+		}
+
 	};
 }
