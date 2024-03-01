8b9192614cbb0437288eab9a64477de28f0326f5
Oláh Levente <levente@stud.uni-obuda.hu>
2024-03-01 14:42:14 +0100

    minor changes

--- CPP_Feladat_01/CPP_Feladat_01/feladat1.cpp
+++ CPP_Feladat_01/CPP_Feladat_01/feladat1.cpp
@@ -2,63 +2,110 @@
 
 double feladat1(const char* s)
 {
-	//const char* k = s + 1;
+	double ans = 0.0;
+	bool positive = true;
+	const char* k = s + 1;
+
 	std::string num = "";
 
 	//trimming WS from the start
 	while (*s < '!')
 	{
 		s++;
-		//k++;
+		k++;
 	}
 
 	//hexa num
 
-	//if (*s == '0' && *k == 'x')
-	if (*s == '0')
+	if (*s == '0' && *k == 'x')
 	{
-		//k++;
-		s++;
-		if (*s == 'x')
+		k++;
+		while (*k >= '0' && *k <= '9' || *k >= 'a' && *k <= 'f' || *k >= 'A' && *k <= 'F')
 		{
-			s++;
-			while (*s >= '0' && *s <= '9' || *s >= 'a' && *s <= 'f' || *s >= 'A' && *s <= 'F')
+			switch (*k)
 			{
-				num += *s;
-				s++;
+			case 'a':
+			case 'A':
+				ans = ans * 16 + 10;
+				break;
+			case 'b':
+			case 'B':
+				ans = ans * 16 + 11;
+				break;
+			case 'c':
+			case 'C':
+				ans = ans * 16 + 12;
+				break;
+			case 'd':
+			case 'D':
+				ans = ans * 16 + 13;
+				break;
+			case 'e':
+			case 'E':
+				ans = ans * 16 + 14;
+				break;
+			case 'f':
+			case 'F':
+				ans = ans * 16 + 15;
+				break;
+			default:
+				ans = ans * 16 + *k - '0';
 			}
+
+			num += *k;
+			k++;
 		}
-		return -5.0;
+		return ans;
 	}
 
 	//dec num
+
 	else
 	{
-
 		if (*s == '-')
 		{
+			positive = false;
+
 			num += '-';
 			s++;
 		}
-		while (*s >= '0' && *s <= '9' || *s != '.')
+		while (*s >= '0' && *s <= '9')
 		{
+			//double
+			ans = ans * 10 + *s - '0';
+
+			//string
 			num += *s;
+
 			s++;
 		}
 		if (*s != '.')
 		{
-			return -5.0;
+			//hiba van, ezért visszatér az addigi eredménnyel
+			return ans;
 		}
 		else
 		{
 			num += *s;
 			s++;
+			int i = 10;
 			while (*s >= '0' && *s <= '9')
 			{
+				int numericForm = (*s - '0');
+
+				double fractionForm = static_cast<double>(numericForm) / i;
+
+				ans = ans + fractionForm;
+				i *= 10;
+
 				num += *s;
 				s++;
 			}
 		}
 	}
-	return -5.0;
+
+	if (positive)
+		return ans;
+	else
+		return -ans;
 }
--- CPP_Feladat_01/CPP_Feladat_01/main.cpp
+++ CPP_Feladat_01/CPP_Feladat_01/main.cpp
@@ -1,10 +1,13 @@
+
+
+
 #include <iostream>
 #include "feladat1.h"
 
 int main() {
 
 	double x;			// ez a példa
-	const char* ertek = "\n-123.34";
+	const char* ertek = " - 0.7";
 
 	x = feladat1(ertek);
 	printf("\n%s -> %f", ertek, x);
