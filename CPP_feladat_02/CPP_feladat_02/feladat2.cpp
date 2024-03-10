ea166db6f4d6e3e9f652070aa6b89fd6ddf7f444
Oláh Levente <levente@stud.uni-obuda.hu>
2024-03-10 11:48:39 +0100

    started 2nd homework

---
+++ CPP_feladat_02/CPP_feladat_02/feladat2.cpp
@@ -0,0 +1,109 @@
+#include "feladat2.h"
+#include <malloc.h>
+
+char* fifoStartPtr = nullptr;
+char* fifoEndPtr = nullptr;
+char* counter = nullptr;
+char* popCounter = nullptr;
+char* pushCounter = nullptr;
+
+int NewFifo(int s) //done
+{
+	if (fifoStartPtr == nullptr || s <= 0) //van már FIFO || s<=0
+	{
+		fifoStartPtr = (char*)malloc(s);
+
+		if (fifoStartPtr == nullptr) // memória allokáció sikertelen
+			return -1;
+
+		fifoEndPtr = fifoStartPtr + s;
+		counter = fifoStartPtr;
+		popCounter = fifoStartPtr;
+		pushCounter = fifoStartPtr;
+		return 0;
+	}
+	return -1;
+}
+
+int DeleteFifo(void) //done
+{
+	if (fifoStartPtr == nullptr) // nincs FIFO
+		return -1;
+
+	free(fifoStartPtr);
+	fifoStartPtr = nullptr;
+	return 0;
+}
+
+int Push(char c)
+{
+	if (fifoStartPtr == nullptr || fifoEndPtr == counter) // nincs FIFO || betelt a FIFO
+		return -1;
+
+
+	*pushCounter = c;
+	pushCounter == fifoEndPtr - 1 ? pushCounter = fifoStartPtr : pushCounter++;
+
+	counter++;
+	return 0;
+}
+
+int Pop(void)
+{
+	if (fifoStartPtr == nullptr || fifoStartPtr == counter) // nincs FIFO || üres a tár
+		return -1;
+
+	char poppedData = *popCounter;
+	popCounter == fifoEndPtr - 1 ? popCounter = fifoStartPtr : popCounter++;
+
+
+	counter--;
+	return poppedData;
+}
+
+int ClearFifo(void) //done
+{
+	if (fifoStartPtr == nullptr) // nincs FIFO
+		return -1;
+
+	int size = GetSize();
+	for (size_t i = 0; i < size; i++)
+	{
+		*(fifoStartPtr + i) = NULL;
+	}
+	counter = fifoStartPtr;
+	return 0;
+}
+
+int GetFree(void) // done
+{
+	if (fifoStartPtr == nullptr) // nincs FIFO
+		return -1;
+	int ans = fifoEndPtr - counter;
+
+	return ans;
+}
+
+int GetSize(void) //done
+{
+	if (fifoStartPtr == nullptr) // nincs FIFO
+		return -1;
+
+	int fee = (int)fifoEndPtr;
+	int foo = (int)fifoStartPtr;
+	int length = fifoEndPtr - fifoStartPtr;
+
+	return length;
+}
+
+int SetSize(int s)
+{
+	char* otherFifoStartPtr = nullptr;
+
+	// nincs FIFO || s<=0 || memória allokáció sikertelen
+	if (fifoStartPtr == nullptr || s <= 0 || !(otherFifoStartPtr = (char*)malloc(s)))
+		return -1;
+
+
+	return 0;
+}
---
+++ CPP_feladat_02/CPP_feladat_02/feladat2.h
@@ -0,0 +1,9 @@
+#pragma once
+int NewFifo(int s);
+int DeleteFifo(void);
+int Push(char c);
+int Pop(void);
+int ClearFifo(void);
+int GetFree(void);
+int GetSize(void);
+int SetSize(int s);
