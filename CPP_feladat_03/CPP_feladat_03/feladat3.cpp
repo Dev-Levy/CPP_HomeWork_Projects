13a4d6cd927e35a08411e379419f3b33514266f3
Oláh Levente <levente@stud.uni-obuda.hu>
2024-03-13 16:25:50 +0100

    serialization

--- CPP_feladat_03/CPP_feladat_03/feladat3.cpp
+++ CPP_feladat_03/CPP_feladat_03/feladat3.cpp
@@ -1,4 +1,7 @@
 #include "feladat3.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
 #define MYBUFSIZE 8192
 #define MYMAXVALUE 65535
 #define MYMAXFN 128
@@ -7,5 +10,30 @@
 
 int sortfile(char* infn, char* outfn, unsigned char* buf, unsigned short bufsize)
 {
-	return 0;
+	//beolvasás
+	ifstream ifs(infn, ios_base::binary); //binary !
+	if (ifs.is_open())
+	{
+		unsigned short n;
+
+		while (ifs.read((char*)&n, sizeof(n))) {
+
+		}
+
+		ifs.close();
+	}
+
+
+
+
+
+	//kiírás
+	ofstream ofs; ofs.open(outfn, ios_base::binary);
+	if (ofs.bad())
+		return(NOK);
+	unsigned short n = 27540;
+
+	ofs.write((char*)&n, sizeof(n));
+
+	ofs.close();
 }
