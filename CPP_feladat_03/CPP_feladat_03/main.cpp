2a12c1731da93802764274392cb6c186b8727736
Oláh Levente <levente@stud.uni-obuda.hu>
2024-03-14 11:29:05 +0100

    desired input done

--- CPP_feladat_03/CPP_feladat_03/main.cpp
+++ CPP_feladat_03/CPP_feladat_03/main.cpp
@@ -1,5 +1,17 @@
 #include "feladat3.h"
+#include <string>
+#include <iostream>
+#include <fstream>
 
-int main() {
+int main(int argc, char* argv[]) {
 
+	char** q;
+	printf("argc: %d\nargv[0]: %s\nArgumentum lista:\n", argc, argv[0]);
+	while (--argc > 0)
+	{
+		printf((argc > 1) ? "%s " : "%s\n", *++argv);
+	}
+
+	std::string filename = argv[0];
+	;
 }
