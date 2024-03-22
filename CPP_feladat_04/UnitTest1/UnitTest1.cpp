a9d7ef30d12fa98eeafac2db5a79fc8171c36a5b
Oláh Levente <levente@stud.uni-obuda.hu>
2024-03-22 11:30:23 +0100

    started testing

--- CPP_feladat_04/UnitTest1/UnitTest1.cpp
+++ CPP_feladat_04/UnitTest1/UnitTest1.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../CPP_feladat_04/CMyDate.h"
+#include "../CPP_feladat_04/CMyDateException.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -8,9 +10,19 @@ namespace UnitTest1
 	TEST_CLASS(UnitTest1)
 	{
 	public:
-		
+
 		TEST_METHOD(TestMethod1)
 		{
+			try
+			{
+				auto asd = CMyDate(1000, 1, 23);
+			}
+			catch (const std::exception& e)
+			{
+				Assert::AreEqual(CMyDateException::ErrMinYear, e.what());
+			}
+
+
 		}
 	};
 }
